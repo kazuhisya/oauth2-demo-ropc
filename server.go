6c00f7abc26822505033972b94a6741b2ad8c8af
Kazuhisa Hara <khara@sios.com>
2019-02-08 19:27:54 +0900

    server: add token verification function
    Signed-off-by: Kazuhisa Hara <khara@sios.com>

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"encoding/json"
 	"gopkg.in/oauth2.v3"
@@ -43,6 +44,18 @@ func main() {
 
 	// Token Store
 	manager := manage.NewDefaultManager()
+
+	// SetPasswordTokenCfg set the password grant token config
+	cfg := &manage.Config{
+		// access token expiration time (default: time.Hour * 2)
+		AccessTokenExp: time.Minute * 2,
+		// refresh token expiration time (default: time.Hour * 24 * 7)
+		RefreshTokenExp: time.Hour * 24 * 7,
+		// whether to generate the refreshing token (default: true)
+		IsGenerateRefresh: false,
+	}
+	manager.SetPasswordTokenCfg(cfg)
+
 	// TODO: imple RDBMS
 	manager.MustTokenStorage(store.NewFileTokenStore("token.db"))
 
@@ -102,5 +115,23 @@ func main() {
 		srv.HandleTokenRequest(w, r)
 	})
 
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		token, err := srv.ValidationBearerToken(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		data := map[string]interface{}{
+			"expires_in": int64(token.GetAccessCreateAt().Add(token.GetAccessExpiresIn()).Sub(time.Now()).Seconds()),
+			"client_id":  token.GetClientID(),
+			"user_id":    token.GetUserID(),
+		}
+		e := json.NewEncoder(w)
+		e.SetIndent("", "  ")
+		e.Encode(data)
+	})
+
+	log.Println("Server is running at 9096 port.")
 	log.Fatal(http.ListenAndServe(":9096", nil))
 }
